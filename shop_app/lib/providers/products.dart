521f17ca6f9a2131173a41fd03ed84a85446a3ec
Henry Joerg <henry.joerg@gmail.com>
2022-07-22 09:08:22 +0200

    code save, chapter 11, part 275

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -65,9 +65,12 @@ class Products with ChangeNotifier {
     );
   }
 
-  Future<void> fetchAndSetProducts() async {
+  // [] for optional, positional argumens
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString =
+        filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : '';
     final url = Uri.parse(
-        'https://flutter-shop-app-5fb83-default-rtdb.firebaseio.com/products.json?auth=$authToken');
+        'https://flutter-shop-app-5fb83-default-rtdb.firebaseio.com/products.json?auth=$authToken&$filterString');
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -109,6 +112,7 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
+          'creatorId': userId,
         }),
       );
       // is wrapped inside a then block, behind the scenes
