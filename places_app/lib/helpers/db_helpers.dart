313389f8a8d8d24446afbabf9eb745a00599ed74
Henry Joerg <henry.joerg@gmail.com>
2022-08-09 13:47:34 +0200

    code save, chapter 13, part 304

---
+++ places_app/lib/helpers/db_helpers.dart
@@ -0,0 +1,34 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DBHelper {
+  static Future<sql.Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+    // opens an existing db or creates a new one
+    return sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)',
+        );
+      },
+      version: 1,
+    );
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    // insert or replace data
+    // static methods are not scoped to the class because of that DBHelper.database()
+    final db = await DBHelper.database();
+    db.insert(
+      table,
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
+  }
+}
